4bd41d0992cb34c5005f42bad1416fedeb463541
Arpit Chaudhary <arpit.pvc2002@gmail.com>
2025-03-11 14:09:16 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,6 @@ const app = require('./src/app')
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+app.listen(1000, () => {
+    console.log('Server is running on http://localhost:1000')
 })
